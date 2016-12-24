b37ded4e61a6bd8270c9b0d037a5411fe5ec0f84
sword4hire <kevingreen424@rocketmail.com>
2016-12-24 14:20:13 +0000

    Add files via upload

---
+++ sketch_dec24a.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // setup comm ports
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // one phrase ooutput, two second delay
+  Serial.write("hello world/n");
+  delay(2000);
+  
+}
